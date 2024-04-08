dca8e481d124761a06574fa441959ab1bed3871a
高巍 <hellboyvto@gmail.com>
2024-04-08 16:01:28 +0800

    add test3.md

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     # 所有配置信息都直接以字典形式保存
     CONFIG = {
         'max_concurrency': 1,
-        'doc_paths': ['test1.md', 'test2.md'],
+        'doc_paths': ['test1.md', 'test2.md', 'test3.md'],
         'target_levels': [3],
         'logger_level': 'DEBUG',
     }
